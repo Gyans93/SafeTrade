d7eeb3f43346c27fda9eba8dfc940478d408058a
omsohdar <omsohdar@gmail.com>
2021-04-24 23:45:40 -0700

    Added config file and parser in __init__.py file

---
+++ BackEnd/__init__.py
@@ -0,0 +1,11 @@
+import configparser
+
+# creating config parser instance
+parser = configparser.ConfigParser()
+
+# reading config file
+parser.read('commonConfig.cfg')
+
+
+if __name__ == "__main__":
+    pass
