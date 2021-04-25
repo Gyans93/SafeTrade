ae2ae5dd946d32f9f0a7f4ca5fa31a38d1d0a4a6
omsohdar <omsohdar@gmail.com>
2021-04-24 19:48:25 -0700

    updated query

--- BackEnd/app.py
+++ BackEnd/app.py
@@ -1,23 +1,39 @@
 from flask import Flask
-import pandas as pd
-from alpha_vantage.timeseries import TimeSeries
+from flask import request, jsonify;
+import pymongo;
 
 app = Flask(__name__)
-ticker = "TSLA"
-keys = "LTR0QUJJTABLTQ11"
-
-def factors(num):
-  return [x for x in range (1, num+1) if num%x==0]
 
 @app.route('/')
-def home():
-  return 0
+def hello_world():
+    return 'Hello, World!'
 
-@app.route('/getStackInfo')
-def getStackInfo():
-    time = TimeSeries(key = keys, output_format = 'pandas')
-    data = time.get_daily(symbol=ticker, outputsize='full')
-    return data
+@app.route('/getStockData', methods=['GET','POST'])
+def getStockData():
+    req = request.get_json();
+    ticker = req.get('ticker');
+    start = req.get('start');
+    end = req.get('end');
+    interval = req.get('interval');
 
-if __name__ == '__main__':
-  app.run()
+    mongoClient = getDBConnection();
+
+    database = "Stocks";
+    db = mongoClient[database];
+    collection = db[ticker];
+    print(ticker);
+    json = collection.find_one({},{"_id":0});
+    print(json);
+    return jsonify(json);
+
+def getDBConnection():
+    client = None;
+    try:
+        client = pymongo.MongoClient("mongodb+srv://dbAdmin:Safetrade@cluster0.rpnkw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+    except Exception as e:
+        print(e);
+
+    return client;
+
+app.run()
+    
